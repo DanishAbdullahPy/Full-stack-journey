b7282177e63f3d3a57cf34e72a91f31e3dfa9fff
Danish Abdullah <danishabdullah276@gmail.com>
2024-03-20 20:17:09 +0530

    Create array.js
    learned about loop and iterlas

---
+++ array.js
@@ -0,0 +1,8 @@
+In this exercise, you must write a for loop that iterates on the myArray variable and prints out all of its members.
+
+var myArray = ["What is the meaning of life?", "The meaning of life is", 42];
+
+for (var i=0; i <myArray.length; i++)
+{
+    console.log(i);
+}
