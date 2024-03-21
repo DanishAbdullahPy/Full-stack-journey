25c84349dd4a24684fa491546218fffa215c1339
Danish Abdullah <danishabdullah276@gmail.com>
2024-03-22 04:32:19 +0530

    Create backgriund.jsx

---
+++ backgriund.jsx
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+import './index.css';
+import background from './components/background';
+background
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div className='relative w-full h-screen bg-zinc-800'>
+      </div>
+    )
+  }
+}
